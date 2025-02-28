4c96ccec2ca807ce480a7460468f2027229c6e93
khalil <52177717+ksmanasra@users.noreply.github.com>
2025-02-27 23:12:26 -0600

    push more changes

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
-# fetch
+# Fetch
 Data Analyst Assessment
 
 Repository includes
 1. README file, which includes answers to assessment
 2. SQL/Python scripts
 3. Visualizations
+
 
 First: Explore the Data
 1. Are there any data quality issues present?
@@ -20,23 +21,40 @@ First: Explore the Data
 
     >Yes. Within the Products file, Category_1, Category_2, Category_3, Category_4 can be more specific and "brand" would be better suited if it were called "product_name" or the like. Within the Transactions file, "quantity" and "sale" are vague, "total quantity" and "total sale" would be more fitting.
 
+
 Second: provide SQL queries (doing this using python+pandas)
 1. Closed-ended questions:
 
     1. What are the top 5 brands by receipts scanned among users 21 and over?
-        >Coca-Cola (628), Annie's (576), Dove (558), Barefoot (552), Oribe (504)
+        >Coca-Cola (288), Annie's (264), Dove (256), Barefoot (253), Oribe (231)
     2. What are the top 5 brands by sales among users that have had their account for at least six months?
-        >Coca-Cola ($2592.10), Annie's ($2383.92), Dove ($2327.47), Barefoot ($2284.59), Oribe ($2085.93)
+        >Coca-Cola ($845.62), Annie's ($776.88), Dove ($756.48), Barefoot ($744.51), Oribe ($679.77)
     3. What is the percentage of sales in the Health & Wellness category by generation?
-        >Centennial (0%), Silent (0%), Boomer (29.87%), Gen-X (20.60%), Millienial (49.53%), Gen-Z (0%)
-        
+        >Centennial (0%), Silent (0%), Boomer (28.13%), Gen-X (31.60%), Millienial (40.27%), Gen-Z (0%)
+
         >[Reference to Year Ranges of Generation Groups](https://libguides.usc.edu/busdem/age)
 
 2. Open-ended questions: for these, make assumptions and clearly state them when answering the question.
 
     1. Who are Fetch’s power users?
-    2. Which is the leading brand in the Dips & Salsa category?
-    3. At what percent has Fetch grown year over year?
+        >Defining "power users" as users who scanned the most receipts, it is a more accurate definition for Fetch versus seeing who bought the most items or who has spent them most. We will go with the theme of the previous questions and list the top 5 users.
 
-Third: communicate with stakeholders
+        >The following output are the top 5 "power users" (there are no names, so we will categorize by the given ID).
+            USER_ID                     SCANNED
+            62925c1be942f00613f7365e    10
+            64e62de5ca929250373e6cf5    10
+            64063c8880552327897186a5     9
+            604278958fe03212b47e657b     7
+            609af341659cf474018831fb     7
+    2. Which is the leading brand in the Dips & Salsa category?
+        > We can define "leading brand" as simply which ever brand sells the most. So we can check through the quantity of each brand sold throughout the dataset.
+
+        >After analysis, we find that Tostitos was the top ranking Dips & Salsa brand, with a total quantity of 20 items.
+    3. At what percent has Fetch grown year over year?
+        >Growth can either be defined by user growth over each year, or the amount of receipts scanned year over year. Since Fetch gathers data it needs from scanned receipts, it would be more significant to use the total amount of receipts scanned each year to understand Fetch's growth.
+
+        >Year over year, based on receipts scanned, Fetch has grown ...
+
+
+Third: Communicate with stakeholders
     ...
--- fetch.py
+++ fetch.py
@@ -3,13 +3,18 @@ import datetime as dt
 
 df_product = pd.read_csv('products.csv') #load product csv
 df_transactions = pd.read_csv('transactions.csv') #load transactions csv
+df_transactions = df_transactions.drop_duplicates(subset = ["RECEIPT_ID"], keep = 'first') #remove duplicate records
 df_users = pd.read_csv('users.csv') #load users csv
+df_users['CREATED_DATE'] = pd.to_datetime(df_users['CREATED_DATE']) #change created date column from str to datetime
 
 firstmerge_df = pd.merge(df_users, df_transactions, left_on = 'ID', right_on = 'USER_ID', how = 'inner') #first merge
-finalmerge_df = pd.merge(firstmerge_df, df_product, on = 'BARCODE', how = 'inner') #final merge, to use in all questions
+
+finalmerge_df = pd.merge(firstmerge_df, df_product, on = 'BARCODE', how = 'inner') #final merge, all three tables
 finalmerge_df['CREATED_DATE'] = pd.to_datetime(finalmerge_df['CREATED_DATE']) #change created date column from str to datetime
 finalmerge_df['BIRTH_DATE'] = pd.to_datetime(finalmerge_df['BIRTH_DATE']) #change birth date column from str to datetime
-finalmerge_df['FINAL_SALE'] = pd.to_numeric(finalmerge_df['FINAL_SALE'], errors = 'coerce') #converts sales figures to numberical values
+finalmerge_df['SCAN_DATE'] = pd.to_datetime(finalmerge_df['SCAN_DATE']) #change scanned date column from str to datetime
+finalmerge_df['FINAL_QUANTITY'] = pd.to_numeric(finalmerge_df['FINAL_QUANTITY'], errors = 'coerce') #converts quantity figures to numerical values
+finalmerge_df['FINAL_SALE'] = pd.to_numeric(finalmerge_df['FINAL_SALE'], errors = 'coerce') #converts sales figures to numerical values
 
 """ 1. What are the top 5 brands by receipts scanned among users 21 and over? """
 def top5brands_21():
@@ -18,7 +23,7 @@ def top5brands_21():
     top_5 = df_brand.nlargest(5) #sorts and filters the top 5 brands
     print(top_5)
 
-top5brands_21()
+#top5brands_21()
 
 ###
 
@@ -29,7 +34,7 @@ def top5brands_6mon():
     top_5 = df_brandbysales.nlargest(5)
     print(top_5)
 
-top5brands_6mon()
+#top5brands_6mon()
 
 ###
 
@@ -39,10 +44,41 @@ def wellness_bygeneration():
     year_ranges = [1901, 1925, 1946, 1965, 1980, 1995, 2012] #list of edges of year ranges
     gens = ['Centennial', 'Silent', 'Boomer', 'Gen-X', 'Millenial', 'Gen-Z'] #generation labels associated with year ranges
     finalmerge_df['Generations'] = pd.cut(finalmerge_df['Year'], bins = year_ranges, labels = gens, right = False) #create column to label user's generation
-    df_health_wellness = finalmerge_df[finalmerge_df['CATEGORY_1'] == 'Health & Wellness']
+    df_health_wellness = finalmerge_df[finalmerge_df['CATEGORY_1'] == 'Health & Wellness'] #filter on health + wellness
     df_generation = df_health_wellness.groupby('Generations')['FINAL_SALE'].sum() #grouping generation by sales
     total_sales = df_health_wellness['FINAL_SALE'].sum() #total sales to use in percentage calculation
     percentage_by_gen = (df_generation / total_sales) * 100 #percentage of sales by generation
     print(percentage_by_gen)
 
-wellness_bygeneration()
+#wellness_bygeneration()
+
+######
+
+""" 4. Who are Fetchs power users? """
+def power_users():
+    df_power = df_transactions.groupby('USER_ID')['RECEIPT_ID'].count() #grouping users by their associated reciepts
+    top5 = df_power.nlargest(5) #filtering top 5 users
+    print(top5)
+
+#power_users()
+
+###
+
+""" 5. Which is the leading brand in the Dips & Salsa category? """
+def salsaverde():
+    df_salsa = finalmerge_df[finalmerge_df['CATEGORY_2'] == 'Dips & Salsa'] #filter on dips+salsa
+    top_dips = df_salsa.groupby('BRAND')['FINAL_QUANTITY'].sum() #aggregate the quantities for each brand
+    big_dipper = top_dips.nlargest(1) #show the top brand
+    print(big_dipper)
+
+#salsaverde()
+
+###
+
+""" 6. At what percent has Fetch grown year over year? """
+def yearly_growth():
+    df_users['Year'] = df_users['CREATED_DATE'].dt.year #make column for year of account creation
+    df_yearly_signups = df_users.groupby('Year')['Year'].count() #counting 
+    print(df_yearly_signups)
+
+yearly_growth()
